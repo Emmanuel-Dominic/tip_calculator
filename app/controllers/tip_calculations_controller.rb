e9ccea14b4778a0304f297b412417ecd90ec0a25
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-12-03 00:57:49 +0300

    implements tip calculator logic

--- app/controllers/tip_calculations_controller.rb
+++ app/controllers/tip_calculations_controller.rb
@@ -21,7 +21,23 @@ class TipCalculationsController < ApplicationController
 
   # POST /tip_calculations or /tip_calculations.json
   def create
-    @tip_calculation = TipCalculation.new(tip_calculation_params)
+    bill_amount = tip_calculation_params[:bill_amount].to_f
+    tip_percentage = tip_calculation_params[:tip_percentage].to_f
+    num_people = tip_calculation_params[:num_people].to_i
+
+    tip_amount = (bill_amount * tip_percentage) / 100
+    total_bill = bill_amount + tip_amount
+    per_person_amount = total_bill / num_people
+
+    @tip_calculation = TipCalculation.new(
+      bill_amount: bill_amount,
+      tip_percentage: tip_percentage,
+      tip_amount: tip_amount,
+      total_bill: total_bill,
+      num_people: num_people,
+      per_person_amount: per_person_amount,
+      timestamp: Time.current
+    )
 
     respond_to do |format|
       if @tip_calculation.save
@@ -36,8 +52,25 @@ class TipCalculationsController < ApplicationController
 
   # PATCH/PUT /tip_calculations/1 or /tip_calculations/1.json
   def update
+    bill_amount = tip_calculation_params[:bill_amount].to_f
+    tip_percentage = tip_calculation_params[:tip_percentage].to_f
+    num_people = tip_calculation_params[:num_people].to_i
+  
+    tip_amount = (bill_amount * tip_percentage) / 100
+    total_bill = bill_amount + tip_amount
+    per_person_amount = total_bill / num_people
+  
+    @tip_calculation.assign_attributes(
+      bill_amount: bill_amount,
+      tip_percentage: tip_percentage,
+      tip_amount: tip_amount,
+      total_bill: total_bill,
+      num_people: num_people,
+      per_person_amount: per_person_amount
+    )
+
     respond_to do |format|
-      if @tip_calculation.update(tip_calculation_params)
+      if @tip_calculation.save
         format.html { redirect_to @tip_calculation, notice: "Tip calculation was successfully updated." }
         format.json { render :show, status: :ok, location: @tip_calculation }
       else
@@ -65,6 +98,6 @@ class TipCalculationsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def tip_calculation_params
-      params.require(:tip_calculation).permit(:bill_amount, :tip_percentage, :tip_amount, :total_bill, :num_people, :per_person_amount, :timestamp)
+      params.require(:tip_calculation).permit(:bill_amount, :tip_percentage, :num_people)
     end
 end
